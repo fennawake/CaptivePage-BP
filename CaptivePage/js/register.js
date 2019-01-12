5d397f242b8ec753fe28aae03cc327539eed87ef
Fernando Souza <fenna.wake@gmail.com>
2019-01-13 00:18:39 +1000

    Style Login page done

---
+++ CaptivePage/js/register.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('form-email');
+      
+      form.addEventListener('submit', function(e) {
+        document.getElementById('status').innerHTML = "";
+        document.getElementById('status').style.display = "none";
+        verifyEmptyEmail();
+        e.preventDefault();
+      })
+
+const verifyEmptyEmail = function() {
+  const email = document.getElementById('email');
+  if (email.value == ''){
+    const status = document.getElementById('status');
+          status.innerHTML = "Enter your email";
+          status.style.display = "table";
+  }else{
+    if (IsEmail){
+      console.log(email.value);
+    }else{
+      console.log("erro");
+    }
+  }
+  
+}
+
+function IsEmail(email){
+  var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
+  var check=/@[w-]+./;
+  var checkend=/.[a-zA-Z]{2,3}$/;
+  if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
+  else {return true;}
+}
