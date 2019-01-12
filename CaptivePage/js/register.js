bf676dd70c994d8d795c148a6c019729ad079803
Fernando Souza <fenna.wake@gmail.com>
2019-01-13 00:23:55 +1000

    Change function validate email

--- CaptivePage/js/register.js
+++ CaptivePage/js/register.js
@@ -14,19 +14,7 @@ const verifyEmptyEmail = function() {
           status.innerHTML = "Please enter your email";
           status.style.display = "table";
   }else{
-    if (IsEmail){
-      console.log(email.value);
-    }else{
-      console.log("erro");
-    }
+    console.log(email.value);
   }
-  
-}
-
-function IsEmail(email){
-  var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
-  var check=/@[w-]+./;
-  var checkend=/.[a-zA-Z]{2,3}$/;
-  if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
-  else {return true;}
+ 
 }
